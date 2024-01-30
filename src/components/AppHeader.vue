7b983d80c36727a6379191ebb45edfbe9a4753fb
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-30 13:22:36 +0100

    fix header navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,124 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      navbar: [
+        {
+          title: "Home",
+          listOne: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+          listTwo: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+        },
+        {
+          title: "Pages",
+          listOne: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+        },
+        {
+          title: "Courses",
+          listOne: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+        },
+        {
+          title: "Features",
+          listOne: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+        },
+        {
+          title: "Blog",
+          listOne: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+        },
+        {
+          title: "Shop",
+          listOne: [
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+            "lorem",
+          ],
+        },
+      ],
+    };
   },
   methods: {},
 };
@@ -11,53 +128,41 @@ export default {
   <header>
     <div class="container">
       <nav>
-        <div
-          class="navbar-container d-flex align-items-center justify-content-between"
-        >
-          <div class="box-left-navbar">
+        <div class="navbar-container d-flex align-items-center justify-content-between ">
+          <div class="box-left-navbar flex-grow-0 ">
             <img src="../../public/img/dark-logo.png" alt="dark-logo.png" />
           </div>
 
-          <div class="box-right-navbar d-flex align-items-center">
+          <div class="box-right-navbar d-flex align-items-center flex-grow-1 ">
             <nav class="navbar navbar-expand-lg navbar">
-              <div class="container-fluid">
-                <button
-                  class="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNavDarkDropdown"
-                  aria-controls="navbarNavDarkDropdown"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div
-                  class="collapse navbar-collapse"
-                  id="navbarNavDarkDropdown"
-                >
-                  <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                      <button
-                        class="btn dropdown-toggle"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        Dropdown
-                      </button>
-                      <ul class="dropdown-menu dropdown-menu-dark">
-                        <li>
-                          <a class="dropdown-item" href="#">Action</a>
-                        </li>
-                      </ul>
-                    </li>
+              <div class="container-fluid"></div>
+              <div>
+                <ul class="d-flex">
+                  <li v-for="(elem, i) in navbar">
+                    {{ elem.title }}
+                  </li>
+                </ul>
+                <div class="d-flex d-none">
+                  <ul class="flex-grow-0">
+                    <li>ciao</li>
                   </ul>
+                  <ul class="flex-grow-0">
+                    <li>ciao</li>
+                  </ul>
+                  <div class="flex-grow-1">
+                    <img
+                      src="../../public/img/homepages-mega-menu-image-alt-300x129.jpg"
+                      alt=""
+                    />
+                  </div>
                 </div>
               </div>
             </nav>
+          </div>
 
+          <div class="box-input input-group d-flex flex-grow-0 ">
             <div class="item-box d-flex justify-content-around">
-              <span class="box-item-shop d-flex ">
+              <span class="box-item-shop d-flex">
                 <div class="dropdown">
                   <button
                     class="btn dropdown-toggle"
@@ -70,12 +175,12 @@ export default {
                   <div class="dropdown-menu cart-box">
                     <div class="dropdown-item mb-5 text-center">
                       <i
-                        class=" fa-brands fa-opencart fa-2xl"
+                        class="fa-brands fa-opencart fa-2xl"
                         style="color: #20ad96"
                       ></i>
                     </div>
-                    <h5 class=" text-center mb-3 ">Your Cart is empty</h5>
-                    <button class="dropdown-item my-button " type="button">
+                    <h5 class="text-center mb-3">Your Cart is empty</h5>
+                    <button class="dropdown-item my-button" type="button">
                       Browse Shop
                     </button>
                   </div>
@@ -94,25 +199,23 @@ export default {
                   </div>
                 </div>
               </span>
-            </div>
-
-            <div class="box-input input-group d-flex">
-              <div class="input-group flex-nowrap">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="search"
-                  aria-describedby="addon-wrapping"
-                />
-                <span class="input-group-text" id="addon-wrapping">
-                  <i
-                    class="fa-solid fa-magnifying-glass"
-                    style="color: #20ad96"
-                  ></i
-                ></span>
-              </div>
+    
+            <div class="input-group flex-nowrap">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="search"
+                aria-describedby="addon-wrapping"
+              />
+              <span class="input-group-text" id="addon-wrapping">
+                <i
+                  class="fa-solid fa-magnifying-glass"
+                  style="color: #20ad96"
+                ></i
+              ></span>
             </div>
           </div>
+        </div>
         </div>
       </nav>
     </div>
@@ -139,19 +242,18 @@ header {
         .box-right-navbar {
           .item-box {
             .box-item-shop {
-              .cart-box{
+              .cart-box {
                 width: 250px;
                 height: 250px;
                 border: 0px solid $white;
                 padding: 50px;
-                .my-button{
+                .my-button {
                   width: 100%;
                   background-color: $green;
                   border: 1px solid;
                   text-align: center;
                 }
               }
-              
             }
           }
         }
