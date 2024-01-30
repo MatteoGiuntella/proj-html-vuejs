9509e8f4a766d09b5671cf31e3f8da40134b1a4b
Passarelli / Francesco Passarelli <passarellif72@gmail.com>
2024-01-30 12:14:07 +0100

    debug starthere.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,9 +13,15 @@ export default {
 
 <template>
     <footer>
-        
+        <ul>
+            <!-- <li>
+                <router-link :to="{ name: 'starthere' }">starthere</router-link>
+            </li> -->
+        </ul>
     </footer>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/main" as *;
+@use "../assets/scss/partials/variables.scss" as *;
 </style>
--- src/pages/starthere.vue
+++ src/pages/starthere.vue
@@ -13,9 +13,201 @@ export default {
 </script>
 
 <template>
-  <h1>parti da qui</h1>
+  <main>
+    <div class="section-array">
+      <div class="container-img text-white debug"></div>
+    </div>
+    <!-- FINE section-array -->
+
+    <div class="container">
+      <div class="section-steps text-center mt-5">
+        <p>
+          PROVA SUBTITLE SECTION STEPS
+        </p>
+        <h2 class="fw-bold mb-5">
+          PROVA SECTION STEPS
+        </h2>
+
+        <div class="section-broadening">
+          
+          <!-- immagine a sinistra -->
+          <div class="row">
+            <div class="col ">
+              <div class="row">
+                <div class="col-6 p-5 border-middle ">
+                  <div class="prova-container position-relative w-100 ">
+                    <h3 class="fw-bold d-flex justify-content-end ">
+                      PROVA img
+                    </h3>
+                    <div class="timeline-dot-right">
+                      <div class="timeline-all-dot-right">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="container-img mt-5 ">
+                    <img src="../../public/img/stock-full-hd-03-480x298.jpg" alt="">
+                  </div>
+                </div>
+                <!-- FINE col -->
+                <div class="col-6 p-5 fw-bold  position-relative">
+                  <div class="container-text">
+                    
+                    <h4 class="fw-bold">
+                      PROVA
+                    </h4>
+                    <p class="mt-4">
+                      Lorem ipsum dolor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quia excepturi quod, aliquid id eaque dignissimos quos mollitia totam saepe. sit amet, consectetur adipisicing elit. Perferendis distinctio error id. Voluptatibus nisi eveniet consequuntur itaque odio similique fugiat quia, architecto corrupti nihil laboriosam eius eum sed consectetur minus delectus reiciendis id amet consequatur? Magnam, porro neque id repudiandae qui laudantium voluptates sunt ipsum dolorum a, itaque harum sapiente.
+                    </p>
+                  </div>
+                </div>
+                <!-- FINE col -->
+              </div>
+              <!-- FINE row -->
+            </div>
+            <!-- FINE col -->
+          </div>
+          <!-- FINE row -->
+
+          <!-- immagine a destra -->
+          <div class="row">
+            <div class="col ">
+              <div class="row">
+                <div class="col-6 border-middle p-5 fw-bold  position-relative">
+                  <div class="container-text">
+                    <h4 class="fw-bold">
+                      PROVA
+                    </h4>
+                    <p class="mt-4">
+                      Lorem ipsum dolor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quia excepturi quod, aliquid id eaque dignissimos quos mollitia totam saepe. sit amet, consectetur adipisicing elit. Perferendis distinctio error id. Voluptatibus nisi eveniet consequuntur itaque odio similique fugiat quia, architecto corrupti nihil laboriosam eius eum sed consectetur minus delectus reiciendis id amet consequatur? Magnam, porro neque id repudiandae qui laudantium voluptates sunt ipsum dolorum a, itaque harum sapiente.
+                    </p>
+                  </div>
+                </div>
+                <!-- FINE col -->
+                <div class="col-6 p-5 ">
+                  <div class="prova-container position-relative w-100 ">
+                    <h3 class="fw-bold d-flex justify-content-start ">
+                      PROVA img
+                    </h3>
+                    <div class="timeline-dot-left">
+                      <div class="timeline-all-dot-left">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="container-img mt-5 ">
+
+                  </div>
+                </div>
+                <!-- FINE col -->
+              </div>
+              <!-- FINE row -->
+            </div>
+            <!-- FINE col -->
+          </div>
+          <!-- FINE row -->
+
+
+          <!-- immagine a sinistra -->
+          <div class="row">
+            <div class="col ">
+              <div class="row">
+                <div class="col-6 p-5 border-middle ">
+                  <div class="prova-container position-relative w-100 ">
+                    <h3 class="fw-bold d-flex justify-content-end ">
+                      PROVA img
+                    </h3>
+                    <div class="timeline-dot-right">
+                      <div class="timeline-all-dot-right">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="container-img mt-5 ">
+
+                  </div>
+                </div>
+                <!-- FINE col -->
+                <div class="col-6 p-5 fw-bold  position-relative">
+                  <div class="container-text">
+                    
+                    <h4 class="fw-bold">
+                      PROVA
+                    </h4>
+                    <p class="mt-4">
+                      Lorem ipsum dolor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quia excepturi quod, aliquid id eaque dignissimos quos mollitia totam saepe. sit amet, consectetur adipisicing elit. Perferendis distinctio error id. Voluptatibus nisi eveniet consequuntur itaque odio similique fugiat quia, architecto corrupti nihil laboriosam eius eum sed consectetur minus delectus reiciendis id amet consequatur? Magnam, porro neque id repudiandae qui laudantium voluptates sunt ipsum dolorum a, itaque harum sapiente.
+                    </p>
+                  </div>
+                </div>
+                <!-- FINE col -->
+              </div>
+              <!-- FINE row -->
+            </div>
+            <!-- FINE col -->
+          </div>
+          <!-- FINE row -->
+
+          <!-- immagine a destra -->
+          <div class="row">
+            <div class="col ">
+              <div class="row">
+                <div class="col-6 border-middle p-5 fw-bold  position-relative">
+                  <div class="container-text">
+                    <h4 class="fw-bold">
+                      PROVA
+                    </h4>
+                    <p class="mt-4">
+                      Lorem ipsum dolor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi quia excepturi quod, aliquid id eaque dignissimos quos mollitia totam saepe. sit amet, consectetur adipisicing elit. Perferendis distinctio error id. Voluptatibus nisi eveniet consequuntur itaque odio similique fugiat quia, architecto corrupti nihil laboriosam eius eum sed consectetur minus delectus reiciendis id amet consequatur? Magnam, porro neque id repudiandae qui laudantium voluptates sunt ipsum dolorum a, itaque harum sapiente.
+                    </p>
+                  </div>
+                </div>
+                <!-- FINE col -->
+                <div class="col-6 p-5 ">
+                  <div class="prova-container position-relative w-100 ">
+                    <h3 class="fw-bold d-flex justify-content-start ">
+                      PROVA img
+                    </h3>
+                    <div class="timeline-dot-left">
+                      <div class="timeline-all-dot-left">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="container-img mt-5 ">
+
+                  </div>
+                </div>
+                <!-- FINE col -->
+              </div>
+              <!-- FINE row -->
+            </div>
+            <!-- FINE col -->
+          </div>
+          <!-- FINE row -->
+          
+
+        </div>
+        <!-- FINE section-broadening -->
+      </div>
+      <!-- FINE section-steps -->
+
+    </div>
+    <!-- FINE section-steps -->
+
+    <div class="section-register">
+      <div class="container text-center">
+        <h2>
+          PROVA REGISTER
+        </h2>
+      </div>
+    </div>
+
+  </main> 
+
+  <footer class=" mt-5 p-5 debug">
+    <div class="container">
+      PROVA FOOTER
+    </div>
+  </footer> 
 </template>
 
 <style lang="scss" scoped>
-@use '../assets/scss/starthere.scss'
+  @use '../assets/scss/starthere.scss';
+  @use '../assets/scss/partials/variables.scss' as *;
 </style>
