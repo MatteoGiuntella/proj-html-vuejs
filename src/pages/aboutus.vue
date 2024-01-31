4750c199f702b24ae10183f50f996b2503e780ed
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-01-31 16:11:35 +0100

    section 3 finita

--- src/pages/aboutus.vue
+++ src/pages/aboutus.vue
@@ -2,6 +2,7 @@
 import { store } from "../store.js";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
+import component2 from "../components/components home page/component2.vue";
 export default {
   data() {
     return {
@@ -31,28 +32,28 @@ export default {
           content:
             "lorem ipsum , Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, enim quos id aperiam soluta accusantium?",
           name: "alex telles",
-          role: "/private tutor",
+          role: "/Private Tutor",
         },
         {
           img: "../../public/img/testimonial-avata-02.jpg",
           content:
             "lorem ipsum , Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, enim quos id aperiam soluta accusantium?",
-          name: "alex telles",
-          role: "/private tutor",
+          name: "Marcus Fletcher",
+          role: "/MArketing",
         },
         {
           img: "../../public/img/testimonial-avata-03.jpg",
           content:
             "lorem ipsum , Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, enim quos id aperiam soluta accusantium?",
-          name: "alex telles",
-          role: "/private tutor",
+          name: "Sally Coiler",
+          role: "/Assistant",
         },
         {
           img: "../../public/img/testimonial-avata-04.jpg",
           content:
             "lorem ipsum , Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, enim quos id aperiam soluta accusantium?",
-          name: "alex telles",
-          role: "/private tutor",
+          name: "Mauro Rossi",
+          role: "/Ceo",
         },
       ],
       slider: [
@@ -80,6 +81,7 @@ export default {
   components: {
     Swiper,
     SwiperSlide,
+    component2
   },
   methods: {
     getImagePath: function (imgPath) {
@@ -151,137 +153,33 @@ export default {
   <!-- section 2 -->
   <section class="section_2">
     <div class="container">
-      <div class="row">
-        <div class="col-6 img_container_section2">
-          <img src="/img/about-us-03-image-01.png" alt="" />
-        </div>
-        <div class="col-6">
-          <div class="text-secondary">
-            DREAM WITH
-            <strong>MAXCOACH</strong>
-          </div>
-          <div class="text-information-container">
-            <span>Construct A </span>
-            <span>Stunning </span> <br />
-            <span>Career Perspective</span>
-          </div>
-
-          <!-- ACCORDION -->
-          <div class="accordion" id="accordionExample">
-            <div class="accordion-item mb-4">
-              <h2 class="accordion-header">
-                <button
-                  class="accordion-button"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
-                  Multiple Platforms Supported for Teaching & Studying
-                </button>
-              </h2>
-              <div
-                id="collapseOne"
-                class="accordion-collapse collapse show"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  <span class="color_dark_gray">
-                    MaxCoach supports the act of teaching and learning on
-                    multiple platforms like online or offline via material
-                    downloads. We know things aren't supposed to be devoured in
-                    a short time, you can always access our knowledge base from
-                    any device.
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item mb-4">
-              <h2 class="accordion-header">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
-                  Multiple Course Participation at the Same Time
-                </button>
-              </h2>
-              <div
-                id="collapseTwo"
-                class="accordion-collapse collapse"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  Study for a number of courses at the same time is possible as
-                  long as you can afford the fee and time for completing them in
-                  the proper time. Learn at your own desired timeline and
-                  timetable. Maximum up to 5 course at a time for a learner at
-                  MaxCoach.
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item mb-4">
-              <h2 class="accordion-header">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
-                  Track Study Progress & Deliver Prompt Feedback
-                </button>
-              </h2>
-              <div
-                id="collapseThree"
-                class="accordion-collapse collapse"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  It's not just a normal thing to be able to build such a solid
-                  online educational system like MaxCoach where your progress is
-                  easily tracked and feedback is properly delivered. We promise
-                  to keep improving our system for better stability and
-                  flexibility.
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Fine accordion -->
-        </div>
-      </div>
+      <component2 />
     </div>
   </section>
 
   <!-- section 3 -->
   <div class="container">
-    <div class="row">
+    <div class="row my-5 ">
       <div class="col-12 text-xl-center">
         <span
-          ><h1>Why Do People <span>*Hearts*</span> Us?</h1></span
+          ><h1>Why Do People <span class="hearts">*Hearts*</span> Us?</h1></span
         >
       </div>
     </div>
     <div class="row justify-content-center">
-      <div class="col-4">
-        <div class="card-img  ">
-          <img class="carousel" :src="card[counter].img" alt="" />
+      <div class="col-5 d-flex ">
+        <div class="card-img d-flex justify-content-center ">
+          <img class=" rounded-circle" :src="card[counter].img" alt="" />
         </div>
       </div>
-      <div class="col-8">
+      <div class="col-5 d-flex flex-column justify-content-center ">
         <p>{{ card[counter].content }}</p>
         <h4>{{ card[counter].name }}</h4>
         <h5>{{ card[counter].role }}</h5>
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 my-5 ">
         <swiper
           :slidesPerView="5"
           :spaceBetween="30"
@@ -338,49 +236,52 @@ section {
     }
   }
 }
-// section 2
-.section_2 {
-  margin-top: 50px;
-  margin-bottom: 50px;
-  .text-information-container {
-    span:first-of-type,
-    span:last-of-type {
-      color: $purple;
-      font-size: 2.7em;
-      font-weight: 600;
-    }
-    span:nth-of-type(2) {
-      color: $green;
-      font-size: 2.7em;
-      font-weight: 300;
-    }
-  }
-  //accordion
-  .accordion-button:not(.collapsed) {
-    background-color: $green;
-    color: white;
-    box-shadow: none;
-  }
+// // section 2
+// .section_2 {
+//   margin-top: 50px;
+//   margin-bottom: 50px;
+//   .text-information-container {
+//     span:first-of-type,
+//     span:last-of-type {
+//       color: $purple;
+//       font-size: 2.7em;
+//       font-weight: 600;
+//     }
+//     span:nth-of-type(2) {
+//       color: $green;
+//       font-size: 2.7em;
+//       font-weight: 300;
+//     }
+//   }
+//   //accordion
+//   .accordion-button:not(.collapsed) {
+//     background-color: $green;
+//     color: white;
+//     box-shadow: none;
+//   }
 
-  .accordion-button.collapsed::after {
-    background: url("/img/plus.svg");
-    background-size: contain;
-  }
+//   .accordion-button.collapsed::after {
+//     background: url("/img/plus.svg");
+//     background-size: contain;
+//   }
 
-  .accordion-button:not(.collapsed)::after {
-    background: url("/img/minus.png");
-    background-size: contain;
-  }
-}
-.img_container_section2 {
-  display: flex;
-  justify-content: center;
-  img {
-    display: block;
-  }
-}
+//   .accordion-button:not(.collapsed)::after {
+//     background: url("/img/minus.png");
+//     background-size: contain;
+//   }
+// }
+// .img_container_section2 {
+//   display: flex;
+//   justify-content: center;
+//   img {
+//     display: block;
+//   }
+// }
 
 // section 3
+.hearts{
+    color: $green;
+}
 .swiper {
   width: 100%;
   height: 100%;
